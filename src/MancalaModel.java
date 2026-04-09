aac98135ad776132d1ddb460adfc37b58f0d2af8
QQChowey <quantang98@gmail.com>
2026-04-09 09:43:50 -0700

    Implement GetStonesFromEnd & GetStonesFromHole

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,7 +1,9 @@
 public class MancalaModel {
+    // constants
     private static int DEFAULT_STARTING_STONES = 4;
     private static int HOLES_PER_SIDE = 6;
 
+    // state of the board
     private int endP1 = 0;
     private int endP2 = 0;
     final private int[] sideP1 = new int[HOLES_PER_SIDE];
@@ -20,17 +22,36 @@ public class MancalaModel {
     }
 
     /**
-     * 1-indexed.
+     * Returns the stones that are in a hole from a player's side. Holes are 1-indexed.
      * @param player {@code int} either 1 or 2 representing Player 1 or Player 2.
-     * @param hole_number {@code int} range [1, 6 or HOLES_PER_SIDE]
+     * @param hole_number {@code int} range [1, 6 or HOLES_PER_SIDE].
      * @return {@code int} stones in that hole.
+     * @throws IllegalArgumentException if player or hole_number arguments are invalid.
      */
-    public int getStones(int player, int hole_number) {
-        if (player != 1 && player != 2)
-            throw new IllegalArgumentException("Invalid player argument; must be 1 or 2.");
+    public int getStonesFromHole(int player, int hole_number) {
         if (hole_number < 1 || hole_number > HOLES_PER_SIDE) {
             throw new IllegalArgumentException("Invalid hole number #" + hole_number +
                     "; must be between 1 and " + HOLES_PER_SIDE + " inclusive.");
         }
+
+        return switch (player) {
+            case 1 -> sideP1[hole_number];
+            case 2 -> sideP2[hole_number];
+            default -> throw new IllegalArgumentException("Invalid player argument; must be 1 or 2.");
+        };
+    }
+
+    /**
+     * Returns the stones in a player's end.
+     * @param player {@code int} either 1 or 2 representing Player 1 or Player 2.
+     * @return {@code int} stones from that player's end
+     * @throws IllegalArgumentException if player is invalid.
+     */
+    public int getStonesFromEnd(int player) {
+        return switch (player) {
+            case 1 -> endP1;
+            case 2 -> endP2;
+            default -> throw new IllegalArgumentException("Invalid player argument; must be 1 or 2.");
+        };
     }
 }
