c07d3c29c9128d952f74f4fd6f5fa228eae10057
heinlin520 <heinlin520@gmail.com>
2026-04-11 22:34:34 -0700

    Update MancalaApp.java
    Added MancalaController so the controller is created

--- src/MancalaApp.java
+++ src/MancalaApp.java
@@ -2,5 +2,7 @@ public class MancalaApp {
     public static void main(String[] args){
         MancalaModel model = new MancalaModel();
         MancalaView view = new MancalaView();
+        MancalaController controller = new MancalaController(model, view);
+
     }
 }
