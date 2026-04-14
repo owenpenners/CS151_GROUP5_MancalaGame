cb59373c338dbaf1f1699937967bd0dd71b04a90
QQChowey <quantang98@gmail.com>
2026-04-14 01:14:12 -0700

    created stone in View

--- src/MancalaView.java
+++ src/MancalaView.java
@@ -61,9 +61,47 @@ public class MancalaView extends JFrame {
         JButton[] buttons = new JButton[6];
 
         for (int i = 0; i < 6; i++) {
-            buttons[i] = new JButton();
+            buttons[i] = createPit(5);
             panel.add(buttons[i]);
         }
         return buttons;
+    }
+
+    private JButton createPit(int stones) {
+        JButton pit = new JButton() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+
+
+                // Paint stones in
+                if (stones == 0) return;
+                int stoneSize = 20;
+                int padding = 8;
+                int cols = (int) Math.ceil(Math.sqrt(stones));
+                int rows = (int) Math.ceil((double) stones / cols);
+
+                int gridW = (getWidth() - 2 * padding) / cols;
+                int gridH = (getHeight() - 2 * padding) / rows;
+
+                int count = 0;
+
+                for (int r = 0; r < rows; r++) {
+                    for (int c = 0; c < cols; c++) {
+                        if (count >= stones) break;
+                        int x = padding + c * gridW + (gridW - stoneSize) / 2;
+                        int y = padding + r * gridH + (gridH - stoneSize) / 2;
+
+                        g2.fillOval(x, y, stoneSize, stoneSize);
+                        count++;
+                    }
+                }
+            }
+        };
+
+
+
+        return pit;
     }
 }
