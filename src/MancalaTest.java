68ce2dc350fa2c6108f7e042f14c9fee0e48c56f
heinlin520 <heinlin520@gmail.com>
2026-04-18 17:15:11 -0700

    Update and rename MancalaApp.java to MancalaTest.java
    To fit the submission description

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -1,4 +1,4 @@
-public class MancalaApp {
+public class MancalaTest {
     public static void main(String[] args){
         MancalaModel model = new MancalaModel();
         MancalaView view = new MancalaView();
